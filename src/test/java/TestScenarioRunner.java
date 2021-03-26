6510e2d6814d87a171263f2081b9a41bb784b0a2
Pete Edwards <edwardsph@users.noreply.github.com>
2021-03-26 16:05:42 +0000

    Add Quarkus and Microprofile-config to fix #10

---
+++ src/test/java/TestScenarioRunner.java
@@ -0,0 +1,29 @@
+import com.intuit.karate.Results;
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.solid.testharness.TestRunner;
+
+import javax.inject.Inject;
+import java.util.Collections;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@Tag("solid")
+@QuarkusTest
+public class TestScenarioRunner {
+    private static final Logger logger = LoggerFactory.getLogger("org.solid.testharness.TestScenarioRunner");
+
+    @Inject
+    TestRunner testRunner;
+
+    @Test
+    void testScenario() {
+        Results results = testRunner.runTests(Collections.singletonList("classpath:content-negotiation/content-negotiation-turtle.feature"));
+        assertNotNull(results);
+        assertEquals(0, results.getFailCount(), results.getErrorMessages());
+    }
+}
